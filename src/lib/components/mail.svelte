e483507bb19bfea45b4f720bdce5ffdcb5c084c4
Simone Simonella <simone.simonella@jakala.com>
2025-02-13 13:30:28 +0100

    added collapse expand function

--- src/lib/components/mail.svelte
+++ src/lib/components/mail.svelte
@@ -1,37 +1,67 @@
 <script lang="ts">
 	import Search from "lucide-svelte/icons/search";
-    import * as Resizable from "$lib/components/ui/resizable/index.js";
-    import * as Tabs from "$lib/components/ui/tabs/index.js";
-    import { Separator } from "$lib/components/ui/separator/index.js";
-    import { Input } from "$lib/components/ui/input/index.js";
+	import * as Resizable from "$lib/components/ui/resizable/index.js";
+	import * as Tabs from "$lib/components/ui/tabs/index.js";
+	import { Separator } from "$lib/components/ui/separator/index.js";
+	import { Input } from "$lib/components/ui/input/index.js";
+    import { cn } from "$lib/utils";
 
-    export let defaultLayout = [265, 440, 655];
+	export let defaultLayout = [265, 440, 655];
+	export let defaultCollapsed = false;
+	export let navCollapsedSize: number;
+
+	let isCollapsed = defaultCollapsed;
+
+	function onLayoutChange(sizes: number[]) {
+		document.cookie = `PaneForge:layout=${JSON.stringify(sizes)}`;
+	}
+
+	function onCollapse() {
+		isCollapsed = true;
+		document.cookie = `PaneForge:collapsed=${true}`;
+	}
+
+	function onExpand() {
+		isCollapsed = false;
+		document.cookie = `PaneForge:collapsed=${false}`;
+	}
 </script>
 
 <div class="md:hidden">TODO: MOBILE</div>
 <div class="hidden md:block h-screen">
-    <Resizable.PaneGroup direction="horizontal">
-        <Resizable.Pane
-            defaultSize={defaultLayout[0]}
-            collapsible
-            minSize={15}
-            maxSize={20}
-        >
-            <div class="flex h-[52px] items-center justify-center">
-                ACCOUNT
-            </div>
-            <Separator />
-        </Resizable.Pane>
-        <Resizable.Handle />
-        <Resizable.Pane defaultSize={defaultLayout[1]} minSize={30}>
-            <Tabs.Root value="all">
+	<Resizable.PaneGroup direction="horizontal" {onLayoutChange}>
+		<Resizable.Pane
+			defaultSize={defaultLayout[0]}
+			collapsedSize={navCollapsedSize}
+			collapsible
+			minSize={15}
+			maxSize={20}
+			{onCollapse}
+			{onExpand}
+		>
+			<div class={cn("flex h-[52px] items-center justify-center",
+				isCollapsed ? "h-[52px]" : "px-2"
+			)}>
+				ACCOUNT
+			</div>
+			<Separator />
+		</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane defaultSize={defaultLayout[1]} minSize={30}>
+			<Tabs.Root value="all">
 				<div class="flex items-center px-4 py-2">
 					<h1 class="text-xl font-bold">Inbox</h1>
 					<Tabs.List class="ml-auto">
-						<Tabs.Trigger value="all" class="text-zinc-600 dark:text-zinc-200">
+						<Tabs.Trigger
+							value="all"
+							class="text-zinc-600 dark:text-zinc-200"
+						>
 							All mail
 						</Tabs.Trigger>
-						<Tabs.Trigger value="unread" class="text-zinc-600 dark:text-zinc-200">
+						<Tabs.Trigger
+							value="unread"
+							class="text-zinc-600 dark:text-zinc-200"
+						>
 							Unread
 						</Tabs.Trigger>
 					</Tabs.List>
@@ -56,11 +86,9 @@
 					<!-- <MailList items={mails.filter((item) => !item.read)} /> -->
 				</Tabs.Content>
 			</Tabs.Root>
-        </Resizable.Pane>
+		</Resizable.Pane>
 
 		<Resizable.Handle withHandle />
-		<Resizable.Pane defaultSize={defaultLayout[2]}>
-            MAIL
-		</Resizable.Pane>
-    </Resizable.PaneGroup>
+		<Resizable.Pane defaultSize={defaultLayout[2]}>MAIL</Resizable.Pane>
+	</Resizable.PaneGroup>
 </div>
