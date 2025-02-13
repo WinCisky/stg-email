f621451013347997399f862de156f6e32b26aa84
WinCisky <xsimone97@hotmail.it>
2025-02-13 22:45:50 +0100

    account switcher

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+import './components/mail.svelte';
 import './components/app-sidebar.svelte';
+import './components/account-switcher.svelte';
+
+import './components/icons/gmail.svelte';
