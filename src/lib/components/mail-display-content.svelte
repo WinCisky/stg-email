3274788287d2a786f175e5c2f9d296361eaecd70
WinCisky <xsimone97@hotmail.it>
2025-03-01 14:24:57 +0100

    added code mirror for src

--- src/lib/components/mail-display-content.svelte
+++ src/lib/components/mail-display-content.svelte
@@ -4,20 +4,55 @@
     import * as Table from "$lib/components/ui/table/index.js";
     import * as Card from "$lib/components/ui/card/index.js";
     import { mailStore } from "$lib/stores/accounts.js";
-    import he from 'he';
+    import he from "he";
+    import { basicSetup, EditorView } from "codemirror";
+    import { EditorState } from "@codemirror/state";
+    import { html } from "@codemirror/lang-html";
 
     import Monitor from "lucide-svelte/icons/monitor";
     import Tablet from "lucide-svelte/icons/tablet";
     import Smartphone from "lucide-svelte/icons/smartphone";
 
     import ShadowContentHtml from "./shadow-content-html.svelte";
+    import { onMount } from "svelte";
 
     let selectedTab: string = $state("html");
     let selectedResolution: "desktop" | "tablet" | "mobile" = $state("desktop");
+    let editorView: EditorView | null = null;
 
     function escapeHTML(text: string) {
         return he.decode(text);
     }
+
+    mailStore.subscribe((value) => {
+        if (editorView) {
+            editorView.dispatch({
+                changes: {
+                    from: 0,
+                    to: editorView.state.doc.length,
+                    insert: value.selected?.html ?? "",
+                },
+            });
+        }
+    });
+
+    onMount(() => {
+        const editor = document.getElementById("editor-code-mirror");
+        if (!editor) {
+            return;
+        }
+        editorView = new EditorView({
+            doc: $mailStore.selected?.html ?? "",
+            parent: editor,
+            extensions: [
+                basicSetup,
+                html(),
+                EditorState.readOnly.of(true),
+                EditorView.editable.of(false),
+                EditorView.contentAttributes.of({ tabindex: "0" }),
+            ],
+        });
+    });
 </script>
 
 <Tabs.Root class="w-full" bind:value={selectedTab}>
@@ -56,7 +91,13 @@
     <Tabs.Content value="html">
         <ShadowContentHtml {selectedResolution} />
     </Tabs.Content>
-    <Tabs.Content value="source">SOURCE CONTENT</Tabs.Content>
+    <Tabs.Content value="source">
+        <Card.Root>
+            <Card.Content>
+                <div id="editor-code-mirror"></div>
+            </Card.Content>
+        </Card.Root>
+    </Tabs.Content>
     <Tabs.Content value="text">TEXT CONTENT</Tabs.Content>
     <Tabs.Content value="raw">
         <Card.Root>
